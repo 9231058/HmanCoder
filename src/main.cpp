332240c81d06959c8becc970fb993380274fbe1f
Parham Alvani <parham.alvani@gmail.com>
2014-12-27 05:43:37 +0330

    Command line argument handling is added.
    This feature add using tclap library.

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,26 @@
  *
  * [] Creation Date : 24-12-2014
  *
- * [] Last Modified : Wed 24 Dec 2014 11:06:27 PM IRST
+ * [] Last Modified : Sat 27 Dec 2014 05:43:06 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
 #include <iostream>
 #include <string>
+#include <tclap/CmdLine.h>
 #include "Nodetype.h"
 #include "MaxHeap.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
+	TCLAP::CmdLine cmd("HmanCoder is simple haffman coder", ' ', "0.0.1");
+	TCLAP::ValueArg<std::string> iPath("i", "input", "Input file", true, "", "string");
+	TCLAP::ValueArg<std::string> oPath("o", "output", "Output file", true, "", "string");
 	
+	cmd.add(iPath);
+	cmd.add(oPath);
+	cmd.parse(argc, argv);
+
+
 }
